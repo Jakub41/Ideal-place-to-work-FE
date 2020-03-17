bd99889529a3fb1453608f93ecc7be5b60528235
henlovelybird <voxylophone@gmail.com>
2020-03-17 16:40:14 +0100

    clean and adding searchbox

--- src/Pages/landingPage/landingIndex.jsx
+++ src/Pages/landingPage/landingIndex.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "./node_modules/react";
-import "./landing.css"
+import React, { Component } from "react";
+import "./Landing.css"
 import Background from "../../images/Betahaus-Cafe-Berlin.jpg";
-import { Col, Row, Container } from "./node_modules/reactstrap";
+import { Col, Row, Container } from "reactstrap";
 import LandingSearch from './LandingComponents/LandingSearch'
 import LandingAPI from "./LandingComponents/LandingAPI"
 import LandingStickyBottom from './LandingComponents/LandingStickyBottom'
@@ -11,13 +11,17 @@ class LandingPage extends Component {
     return (<>
       <Container fluid style={{ height: "100vh" }}>
         <Row className={"image"}>
-          <Col xs="12">
-            <img src={Background} alt={"cafebackground"} />
+          
+            <img src={Background} alt="cafebackground" />
+            
             <h1>
               Your next <br /> perfect place <br /> to work
             </h1>
-          </Col>
+            <LandingSearch />
         </Row>
+
+       
+
       </Container>
 
 
