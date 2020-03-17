ae7224d75bf6c22c5b58ee4efc33d085d172f5f9
henlovelybird <voxylophone@gmail.com>
2020-03-17 11:41:05 +0100

    PascalCasing
    (cherry picked from commit 90b39f68420a588855d7e9735966f87f37089bca)

--- src/Pages/landingPage/landingComponents/landingSearch.jsx
+++ src/Pages/landingPage/landingComponents/landingSearch.jsx
@@ -5,14 +5,29 @@ class LandingSearch extends Component {
     render() {
         return (
             <div id="SearchDiv">
+                import React, {Component} from 'react';
 
-                <Form >
-                <Input className="searchInput" type="search" placeholder="wifi cafe near me" aria-label="Search" />
-                <Button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</Button>
+                <Form>
+                    <Input className="searchInput" type="search" placeholder="wifi cafe near me" aria-label="Search"/>
+                    <Button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</Button>
                 </Form>
 
-                
 
+            </div>
+        );
+    }
+}
+
+export default LandingSearch;
+
+class LandingSearch extends Component {
+    render() {
+        return (
+            <div>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
             </div>
         );
     }
