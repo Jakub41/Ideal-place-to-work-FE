d514c88949b9041793adb79cb5f62df7f8a39b2f
Aby Babu <abyStriveschool@gmail.com>
2020-03-18 23:26:00 +0100

    detailsPage

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import LandingPage from "../Pages/landingPage/landingIndex";
+import LandingPage from "../Pages/landingPage/LandingIndex";
+import Footer from "../Pages/landingPage/landingComponents/Footer";
 
 class Main extends Component {
     render() {
         return (
             <div>
-                <LandingPage/>
+                {/*<LandingPage/>*/}
+                <DetailsPageIndex/>
+                <Footer/>
             </div>
         );
     }
