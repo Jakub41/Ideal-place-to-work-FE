055056ec0fac44bb1ea730494336d3394cb1b0f7
henlovelybird <voxylophone@gmail.com>
2020-03-24 14:58:19 +0100

    favorite page draft

--- src/MainIndex.js
+++ src/MainIndex.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LandingPage from "./Pages/landingPage/LandingIndex";
 import Footer from "./Pages/landingPage/landingComponents/Footer";
 import DetailsPageLanding from './Pages/detailsPage/DetailsPageIndex';
-
+import FavoritesPageIndex from './Pages/favoritesPage/FavoritesPageIndex'
 
 class Main extends Component {
     render() {
@@ -13,6 +13,7 @@ class Main extends Component {
                 <Switch>
                 <Route exact path="/"><LandingPage/></Route>
                 <Route path="/details"><DetailsPageLanding /></Route>
+                <Route path="/favs"><FavoritesPageIndex/></Route>
                 </Switch>
                 <Footer/>
             </Router>
