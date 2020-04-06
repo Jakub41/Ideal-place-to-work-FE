6ecf2e3c15ba50c5be7994f712137b391f2b273f
fainapahanko <f.poganko@gmail.com>
2020-04-06 15:36:50 +0200

    comments modal done

--- src/Pages/rating& review/reviewComponents/ReviewModal.jsx
+++ src/Pages/rating& review/reviewComponents/ReviewModal.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Button, Col, Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader, Row} from 'reactstrap';
+import { Col, Row, Button } from 'reactstrap';
+import StarRatings from 'react-star-ratings';
+import { withRouter } from "react-router";
 import Api from '../../../Api';
-import RatingStars from "./RatingStars";
 import Coffee from "../../../icons/bars01.png";
 import ComfyPlace from "../../../icons/cowos01.png";
+import closeIcon from "../../../icons/close.png";
 import WiFi from "../../../icons/wifi01.png";
 import "../Rating.css"
 import NotLoggedIn from "../../detailsPage/detailsComponents/NotLoggedInMsgDivBox"
@@ -11,138 +13,197 @@ import "../../detailsPage/detailsComponents/NotLoggedIn.css"
 
 
 class CommentModal extends React.Component {
-    //const {buttonLabel, className} = props;
-    //experience = JSON.parse(JSON.stringify(this.props.experience));
-
+    
     constructor(props) {
         super(props);
-        this.state = {modal: false, alertMsgDiv: false, showAlertMsg: false, selectedFile: null, comment: {comment: ""}};
+        this.state = { 
+            modal: false, 
+            alertMsgDiv: false, 
+            showAlertMsg: false, 
+            selectedFile: null, 
+            comment: { 
+                  Text: "",
+                  Rating: 0,
+                  GoodService: 0,
+                  WifiRate: 0,
+                  QuitePlace: 0,
+            },
+        };
     }
 
-    submit = (e) => {
-        if (this.state.comment._id) {
-            Api.fetch("/reviews/:reviewId" + this.props.placeId, "PATCH", JSON.stringify(this.state.comment),
-                {"Authorization": "Bearer " + localStorage.getItem("access_token")}
-        ).then(res => {
+    handleText = (e) => {
+        console.log(e.target.value)
+        const text = e.target.value
+        this.setState(prevState => ({
+            comment: {                   // object that we want to update
+                ...prevState.comment,    // keep all other key-value pairs
+                Text: text      // update the value of specific key
+            }
+        }))
+    }
 
-                console.log("edit", res);
-                // this.props.refresh();
-            });
+    changeRating = ( newRating, name ) => {
+        this.setState(prevState => ({
+            comment: {                   // object that we want to update
+                ...prevState.comment,    // keep all other key-value pairs
+                [name]: newRating       // update the value of specific key
+            }
+        }))
+    }
 
-        } else {
-            Api.fetch("/reviews/" + this.props.placeId,
-                "POST",
-                JSON.stringify(this.state.comment),
-                {"Authorization": "Bearer " + localStorage.getItem("access_token")}).then(res => {
-                console.log("inserted", res);
-                // this.props.refresh()
-            });
-        }
-        this.setState({_id: undefined});
-        this.toggle();
-    };
+    submitComment = async() => {
+        console.log(this.state.comment)
+        let req = await Api.fetch(`/reviews/${this.props.match.params.id}`, "POST", JSON.stringify(this.state.comment), {"Authorization": "Bearer " + localStorage.getItem("access_token")})
+        console.log(req);
+    }
 
     toggleModalOrAlert = async () => {
         const token = localStorage.getItem("access_token")
-        console.log("hey", token)
-        if(token !== "undefined") {
-            console.log("hey im here and i have a token!")
-            this.setState({modal: !this.state.modal})
-        } else if(token === "undefined" ){
-            console.log("hey im here and i don't have a token!")
-            this.setState({alertMsgDiv: !this.state.alertMsgDiv})
+        if (token !== "undefined" && token !== "" && token) {
+            console.log("token")
+            this.setState({ modal: !this.state.modal })
+        } else {
+            console.log("no token")
+            this.setState({ alertMsgDiv: !this.state.alertMsgDiv })
         }
-        console.log(this.state)
     };
 
     toggleShowAlertMsg = (e) => {
-        this.setState({alertMsgOpen: true})
-      };
-
-     onChangeHandler = event => {
-        console.log(event.target.files[0]);
-        this.setState({
-            selectedFile: event.target.files[0],
-            loaded: 0,
-        })
-    };
-
-    updateForm = (e) => {
-        this.setState({comment: {...this.state.comment, [e.target.id]: e.target.value}});
-    };
-
-    addEmoticon = (e) => {
-        e.persist();
-        console.log(e);
-        this.setState({comment: {comment: this.state.comment.comment + e.target.innerText}});
+        this.setState({ alertMsgOpen: true })
     };
 
     render() {
-        const props = this.props;
         return (<>
-                <div id="please-login-reg-div" className="cursor" onClick={() => {this.toggleModalOrAlert(); 
-                    this.toggleShowAlertMsg()}} >
-                    <h2 className='rate-place'>Rate Place</h2>
-                </div>
-                
-                {this.state.alertMsgDiv &&  <NotLoggedIn alertMsgOpen={this.alertMsgOpen} />} 
-                {this.state.modal && 
-                <Modal style={{borderRadius: '30px'}} isOpen={this.state.modal} 
-                    toggle={this.toggleModalOrAlert} className={this.props.className}>
-                    <ModalHeader toggle={this.toggleModalOrAlert}>Rate this Place</ModalHeader>
-                    <ModalBody>
-                        <Form>
-                            <Row form>
-                                <Col md={12}>
-                                    <FormGroup>
-                                        <div>
-                                            <div className="star-rating-div">
-                                                <RatingStars placeId={this.props.placeId}/>
-                                            </div>
+            <div id="please-login-reg-div" className="cursor" onClick={this.toggleModalOrAlert} >
+                <h2 className={this.state.alertMsgDiv ? 'rate-place-alert' : 'rate-place'}>Rate Place</h2>
+            </div>
 
-                                                <div className="comment-icon-container">
-                                                    <div className="comment-icon-container1">
-                                                        <img src={Coffee} alt="Coffie" className="coffee-icon"/>
-                                                        <p className="coffee-text">Good Services</p>
-                                                    </div>
-                                                    <div className="comment-icon-container2">
-                                                        <img src={ComfyPlace} alt="comfyPlace" className="comfyPlace-icon"/>
-                                                        <p className="comfyPlace-text">Comfortable Place</p>
-                                                    </div>
-                                                    <div className="comment-icon-container3">
-                                                        <img src={WiFi} alt="WiFi" className="wifi-icon"/>
-                                                        <p className="wifi-text">Strong Wi-fi</p>
-                                                    </div>
-                                                </div>
-                                        </div>
-                                        <Label for='startDate'>Write your comment</Label>
-                                        <Input type="textarea" onChange={this.updateForm} name="Text" id="Text"
-                                               cols="80"
-                                               rows="10" value={this.state.comment.Text}></Input>
-                                    </FormGroup>
-                                </Col>
-                            </Row>
-                            <Row form>
-                                <span style={{cursor: 'pointer'}} onClick={this.addEmoticon}>😀</span>
-                                <span style={{cursor: 'pointer'}} onClick={this.addEmoticon}>🤣</span>
-                                <span style={{cursor: 'pointer'}} onClick={this.addEmoticon}>😇</span>
-                                <span style={{cursor: 'pointer'}} onClick={this.addEmoticon}>😅</span>
-                                <span style={{cursor: 'pointer'}} onClick={this.addEmoticon}>😜</span>
-                                <span style={{cursor: 'pointer'}} onClick={this.addEmoticon}>😩</span>
-                                <span style={{cursor: 'pointer'}} onClick={this.addEmoticon}>😭</span>
-                                <span style={{cursor: 'pointer'}} onClick={this.addEmoticon}>😡</span>
-                            </Row>
-                        </Form>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color='primary' onClick={this.submit.bind(this)}>
-                            Submit
-                        </Button>
-                    </ModalFooter>
-                </Modal>} 
-            </>
+            {this.state.alertMsgDiv && <NotLoggedIn alertMsgOpen={this.alertMsgOpen} />}
+            {this.state.modal && <div className={this.state.modal ? "review-modal-open" : "review-modal-close"}>
+            <div>
+                <h2 className="modal-rev-header">
+                    How would you <br />
+              Rate this place?
+            </h2>
+                <img
+                    src={closeIcon}
+                    onClick={this.toggleModalOrAlert}
+                    className="exit-icon-modal"
+                    alt="searchIcon"
+                />
+            </div>
+                <Row>
+                    <Col className="col-12">
+                        <Row className='mt-4'>
+                            <Col className="col-3 mt-3">
+                                <img
+                                    src={WiFi}
+                                    className="rating-icon-modal"
+                                    alt="searchIcon"
+                                />
+                            </Col>
+                            <Col className="col-9 mt-3">
+                                <Row>
+                                    <Col className="col-12">
+                                        <h3>Was the wifi good enough?</h3>
+                                    </Col>
+                                    <Col className="col-12">
+                                        <StarRatings
+                                        rating={this.state.comment.WifiRate}
+                                        starRatedColor="#8806C1"
+                                        changeRating={this.changeRating}
+                                        numberOfStars={5}
+                                        isSelectable={true}
+                                        name='WifiRate'
+                                        starDimension="30px"
+                                        starSelectingHoverColor="#8806C1"
+                                        />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col className="col-3 mt-3">
+                                <img
+                                    src={Coffee}
+                                    className="rating-icon-modal"
+                                    alt="searchIcon"
+                                />
+                            </Col>
+                            <Col className="col-9 mt-3">
+                                <Row>
+                                    <Col className="col-12">
+                                        <h3>Was the service good enough?</h3>
+                                    </Col>
+                                    <Col className="col-12">
+                                        <StarRatings
+                                        rating={this.state.comment.GoodService}
+                                        starRatedColor="#8806C1"
+                                        changeRating={this.changeRating}
+                                        numberOfStars={5}
+                                        isSelectable={true}
+                                        name='GoodService'
+                                        starDimension="30px"
+                                        starSelectingHoverColor="#8806C1"
+                                        />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col className="col-3 mt-3">
+                                <img
+                                    src={ComfyPlace}
+                                    className="rating-icon-modal"
+                                    alt="searchIcon"
+                                />
+                            </Col>
+                            <Col className="col-9 mt-3">
+                                <Row>
+                                    <Col className="col-12">
+                                        <h3>Was the place quiet enough?</h3>
+                                    </Col>
+                                    <Col className="col-12">
+                                        <StarRatings
+                                        rating={this.state.comment.QuitePlace}
+                                        starRatedColor="#8806C1"
+                                        changeRating={this.changeRating}
+                                        numberOfStars={5}
+                                        isSelectable={true}
+                                        name='QuitePlace'
+                                        starDimension="30px"
+                                        starSelectingHoverColor="#8806C1"
+                                        />
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col className="col-4 mt-3 ml-4">
+                                <h3>General impression?</h3>
+                            </Col>
+                            <Col className="col-9 mt-3 ml-4">
+                                <StarRatings
+                                    rating={this.state.comment.Rating}
+                                    starRatedColor="#8806C1"
+                                    changeRating={this.changeRating}
+                                    numberOfStars={5}
+                                    isSelectable={true}
+                                    name='Rating'
+                                    starDimension="30px"
+                                    starSelectingHoverColor="#8806C1"
+                                    />
+                            </Col>
+                            <Col className='col-12 mt-3 ml-4'>
+                                <form className="form-container">
+                                    <label>Leave a comment</label>
+                                    <br/>
+                                    <textarea onChange={this.handleText}
+                                    cols="10" rows="50"></textarea>
+                                </form>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Button onClick={this.submitComment} className='ml-5 mt-2 '>Submit</Button>
+                </Row>
+            </div>}
+        </>
         );
-    }
+}
 }
 
-export default CommentModal;
+export default withRouter(CommentModal);
