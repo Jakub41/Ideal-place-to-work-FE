269f7332bbddf82944ea1260a97756786b1bb796
fainapahanko <f.poganko@gmail.com>
2020-04-10 15:49:44 +0200

    fix loaders

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import {ajax} from "rxjs/ajax";
 //This returning a promise that will be resolved with the data
 export class Api {
-    static URL = "http://localhost:9100/api/v1";
+    static URL = "https://ideal-place-to-work.herokuapp.com/api/v1";
     static fetch = (url, method = "GET", body, headers) => {
         return ajax( {
             url: Api.URL + url,
